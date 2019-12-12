428f758ed86c7c8ea4a3737da3a6d6b1a870cd5f
Alokcbre <alok.pandey2@cbre.com>
2019-12-13 00:20:20 +0530

    po

--- appcenter-post-build.sh
+++ appcenter-post-build.sh
@@ -27,7 +27,7 @@ echo ""
 
 echo "> Run UI test command"
 # Note: must put a space after each parameter/value pair
-appcenter test run xcuitest --app $appName --devices $deviceSetName --app-path $APPCENTER_OUTPUT_DIRECTORY/UiTest.ipa --test-series $testSeriesName --locale "en_US" --build-dir $APPCENTER_SOURCE_DIRECTORY/UiTest.xcodeproj --token $appCenterLoginApiToken
+appcenter test run xcuitest --app $appName --devices $deviceSetName --app-path $APPCENTER_OUTPUT_DIRECTORY/UiTest.ipa --test-series $testSeriesName --locale "en_US" --build-dir $APPCENTER_SOURCE_DIRECTORY/UiTest/Base.lproj --token $appCenterLoginApiToken
 
 echo ""
 echo "**************************************************************************************************"
