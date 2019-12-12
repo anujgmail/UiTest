1814ab0d2d5f0e818cb48c9568c0d1fdac6e8f39
Alokcbre <alok.pandey2@cbre.com>
2019-12-12 17:04:47 +0530

    Added file

---
+++ UiTest/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  UiTest
+//
+//  Created by Alok Pandey on 12/12/19.
+//  Copyright © 2019 CBRE. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ UiTestUITests/UiTestUITests.swift
@@ -0,0 +1,48 @@
+//
+//  UiTestUITests.swift
+//  UiTestUITests
+//
+//  Created by Alok Pandey on 12/12/19.
+//  Copyright © 2019 CBRE. All rights reserved.
+//
+
+import XCTest
+
+class UiTestUITests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+
+        // In UI tests it is usually best to stop immediately when a failure occurs.
+        continueAfterFailure = false
+
+        // In UI tests it’s important to set the initial state - such as interface orientation - required for your tests before they run. The setUp method is a good place to do this.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // UI tests must launch the application that they test.
+        let app = XCUIApplication()
+        app.launch()
+
+        let button = XCUIApplication().buttons["Button"]
+        sleep(2)
+        button.tap()
+        sleep(2)
+
+        // Use recording to get started writing UI tests.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testLaunchPerformance() {
+        if #available(macOS 10.15, iOS 13.0, tvOS 13.0, *) {
+            // This measures how long it takes to launch your application.
+            measure(metrics: [XCTOSSignpostMetric.applicationLaunch]) {
+                XCUIApplication().launch()
+            }
+        }
+    }
+}
