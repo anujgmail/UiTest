bd4ec85c42b89f55f1df3cceb3cc635896e38f2c
Alokcbre <alok.pandey2@cbre.com>
2019-12-12 17:22:02 +0530

    Updated app

--- UiTestUITests/UiTestUITests.swift
+++ UiTestUITests/UiTestUITests.swift
@@ -28,11 +28,6 @@ class UiTestUITests: XCTestCase {
         let app = XCUIApplication()
         app.launch()
 
-        let button = XCUIApplication().buttons["Button"]
-        sleep(2)
-        button.tap()
-        sleep(2)
-
         // Use recording to get started writing UI tests.
         // Use XCTAssert and related functions to verify your tests produce the correct results.
     }
