2758c1a9b86ffc23425959c3cb7d267c82cd17db
Alokcbre <alok.pandey2@cbre.com>
2019-12-13 14:27:54 +0530

    pass test case

--- UiTestUITests/UiTestUITests.swift
+++ UiTestUITests/UiTestUITests.swift
@@ -31,7 +31,6 @@ class UiTestUITests: XCTestCase {
         sleep(1)
         app.buttons["Button"].tap()
         sleep(1)
-        app.buttons["Button"].tap()
 
         // Use recording to get started writing UI tests.
         // Use XCTAssert and related functions to verify your tests produce the correct results.
