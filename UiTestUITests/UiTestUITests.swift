bdcf135b7176b0cf2cc304c91c6a08888c13f15a
Alokcbre <alok.pandey2@cbre.com>
2019-12-13 20:26:13 +0530

    uuuuuu

--- UiTestUITests/UiTestUITests.swift
+++ UiTestUITests/UiTestUITests.swift
@@ -10,23 +10,17 @@ import XCTest
 
 class UiTestUITests: XCTestCase {
 
+    private var app: XCUIApplication!
+
     override func setUp() {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
-
-        // In UI tests it is usually best to stop immediately when a failure occurs.
-        continueAfterFailure = false
-
-        // In UI tests it’s important to set the initial state - such as interface orientation - required for your tests before they run. The setUp method is a good place to do this.
+        app = XCUIApplication()
+        app.launch()
     }
 
     override func tearDown() {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        app = nil
     }
-
     func testExample() {
-
-        let app = XCUIApplication()
-        app.launch()
         //sleep(2)
         app.buttons["Button"].tap()
         //sleep(2)
@@ -46,9 +40,7 @@ class UiTestUITests: XCTestCase {
     }
 
     func testSecond() {
-        let app = XCUIApplication()
-        app.launch()
-        //sleep(2)
+        
         app.buttons["Button"].tap()
         //sleep(2)
         app.buttons["Button2"].tap()
