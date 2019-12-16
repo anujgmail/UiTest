dd76b26b9b4a3ec4707138c39bbfbc6e348fdc14
Alokcbre <alok.pandey2@cbre.com>
2019-12-16 12:44:43 +0530

    Script

---
+++ appcenter-post-build.sh
@@ -0,0 +1,35 @@
+#!/usr/bin/env bash
+
+# Post Build Script
+
+set -e # Exit immediately if a command exits with a non-zero status (failure)
+
+echo "**************************************************************************************************"
+echo "Post Build Script"
+echo "**************************************************************************************************"
+
+##################################################
+# Start UI Tests
+##################################################
+
+# variables
+appCenterLoginApiToken="8a09978a-09a0-4dfa-9da8-76d581a86236" # this comes from the build environment variables
+appName="alok.pandey2/UiTest"
+deviceSetName="alok.pandey2/hostdevice"
+testSeriesName="master"
+
+echo ""
+echo "Start XCUITest run"
+echo "   App Name: $appName"
+echo " Device Set: $deviceSetName"
+echo "Test Series: $testSeriesName"
+echo ""
+
+echo "> Run UI test command"
+# Note: must put a space after each parameter/value pair
+appcenter test run xcuitest --app $appName --devices $deviceSetName --app-path $APPCENTER_OUTPUT_DIRECTORY/UiTest.ipa --test-series $testSeriesName --locale "en_US" --build-dir $APPCENTER_SOURCE_DIRECTORY/ --token $appCenterLoginApiToken
+
+echo ""
+echo "**************************************************************************************************"
+echo "Post Build Script complete"
+echo "**************************************************************************************************"
