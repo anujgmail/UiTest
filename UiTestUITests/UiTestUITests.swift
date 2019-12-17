3882f8fa044e4836e6bc8ec07e6886e594de7580
Alokcbre <alok.pandey2@cbre.com>
2019-12-17 12:08:16 +0530

    Changed Post script

--- UiTestUITests/UiTestUITests.swift
+++ UiTestUITests/UiTestUITests.swift
@@ -49,40 +49,6 @@ class UiTestUITests: XCTestCase {
         asdf.typeText("asdfasfaasfasfasfa")
 
         app.buttons["Button3"].tap()
-
-
-        let app = XCUIApplication()
-        app.buttons["Button"].tap()
-        app.buttons["Button2"].tap()
-        app.textFields["textfield"].tap()
-
-        let uKey = app/*@START_MENU_TOKEN@*/.keys["u"]/*[[".keyboards.keys[\"u\"]",".keys[\"u\"]"],[[[-1,1],[-1,0]]],[0]]@END_MENU_TOKEN@*/
-        uKey.tap()
-        uKey.tap()
-        
-        let yKey = app/*@START_MENU_TOKEN@*/.keys["y"]/*[[".keyboards.keys[\"y\"]",".keys[\"y\"]"],[[[-1,1],[-1,0]]],[0]]@END_MENU_TOKEN@*/
-        yKey.tap()
-        yKey.tap()
-
-        let gKey = app/*@START_MENU_TOKEN@*/.keys["g"]/*[[".keyboards.keys[\"g\"]",".keys[\"g\"]"],[[[-1,1],[-1,0]]],[0]]@END_MENU_TOKEN@*/
-        gKey.tap()
-        gKey.tap()
-        gKey.tap()
-
-        let fKey = app/*@START_MENU_TOKEN@*/.keys["f"]/*[[".keyboards.keys[\"f\"]",".keys[\"f\"]"],[[[-1,1],[-1,0]]],[0]]@END_MENU_TOKEN@*/
-        fKey.tap()
-        fKey.tap()
-
-        let hKey = app/*@START_MENU_TOKEN@*/.keys["h"]/*[[".keyboards.keys[\"h\"]",".keys[\"h\"]"],[[[-1,1],[-1,0]]],[0]]@END_MENU_TOKEN@*/
-        hKey.tap()
-        hKey.tap()
-        app/*@START_MENU_TOKEN@*/.buttons["Return"]/*[[".keyboards",".buttons[\"return\"]",".buttons[\"Return\"]"],[[[-1,2],[-1,1],[-1,0,1]],[[-1,2],[-1,1]]],[0]]@END_MENU_TOKEN@*/.tap()
-
-        let element = app.children(matching: .window).element(boundBy: 0).children(matching: .other).element.children(matching: .other).element(boundBy: 1).children(matching: .other).element.children(matching: .other).element.children(matching: .other).element
-        element.tap()
-        element.tap()
-        element.tap()
-        element.tap()
         
     }
 }
--- appcenter-post-build.sh
+++ appcenter-post-build.sh
@@ -27,7 +27,7 @@ echo ""
 
 echo "> Run UI test command"
 # Note: must put a space after each parameter/value pair
-appcenter test run xcuitest --app $appName --devices $deviceSetName --app-path $APPCENTER_OUTPUT_DIRECTORY/UiTest.ipa --test-series $testSeriesName --locale "en_US" --build-dir $APPCENTER_SOURCE_DIRECTORY/ --token $appCenterLoginApiToken
+appcenter test run xcuitest --app $appName --devices $deviceSetName --app-path $APPCENTER_OUTPUT_DIRECTORY/UiTest.ipa --test-series $testSeriesName --locale "en_US" --build-dir $APPCENTER_SOURCE_DIRECTORY/UiTest.UITests/bin/Debug --uitest-tools-dir $APPCENTER_SOURCE_DIRECTORY/packages/XCUITest/tools --token $appCenterLoginApiToken
 
 echo ""
 echo "**************************************************************************************************"
