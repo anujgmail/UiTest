94e6a7cafc85195874507017d000ab6edad17f45
Alokcbre <alok.pandey2@cbre.com>
2019-12-26 13:11:29 +0530

    Failed test case

--- UiTestUITests/UiTestUITests.swift
+++ UiTestUITests/UiTestUITests.swift
@@ -28,8 +28,7 @@ class UiTestUITests: XCTestCase {
         let app = XCUIApplication()
         app.launch()
 
-        // Use recording to get started writing UI tests.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
+        XCTFail("UI Test Failed")
     }
 
     func testLaunchPerformance() {
