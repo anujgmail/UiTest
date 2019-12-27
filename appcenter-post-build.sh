9b022ae638919e078c50146a58903136c73869dd
Alokcbre <alok.pandey2@cbre.com>
2019-12-27 17:57:30 +0530

    Script

--- appcenter-post-build.sh
+++ appcenter-post-build.sh
@@ -39,27 +39,27 @@ echo "> Run UI test command"
 ##################################################
 
   curl -X POST \
-  >  'https://automation.codebarrel.io/pro/hooks/7945b3d5ad2d517b06383b712d2a99eae274208c?issue=TEST-123&Content-Type=application/json' \
-  >  -H 'Accept: */*' \
-  >  -H 'Accept-Encoding: gzip, deflate' \
-  >  -H 'Cache-Control: no-cache' \
-  >  -H 'Connection: keep-alive' \
-  >  -H 'Content-Length: 175' \
-  >  -H 'Content-Type: text/plain' \
-  >  -H 'Host: automation.codebarrel.io' \
-  >  -H 'Postman-Token: 1935b69c-2fd6-4fff-b584-0458608199b8,5c9278d1-faba-40cb-ac96-6d2f3d2aa906' \
-  >  -H 'User-Agent: PostmanRuntime/7.18.0' \
-  >  -H 'cache-control: no-cache' \
-  >  -d '{
-  >   "fields": {
-  >     "environment": "Env",
-  >     "description": "Description here",
-  >     "labels": [
-  >       "bugfix",
-  >       "blitz_test"
-  >     ]
-  >   }
-  > }'
+    'https://automation.codebarrel.io/pro/hooks/7945b3d5ad2d517b06383b712d2a99eae274208c?issue=TEST-123&Content-Type=application/json' \
+    -H 'Accept: */*' \
+    -H 'Accept-Encoding: gzip, deflate' \
+    -H 'Cache-Control: no-cache' \
+    -H 'Connection: keep-alive' \
+    -H 'Content-Length: 175' \
+    -H 'Content-Type: text/plain' \
+    -H 'Host: automation.codebarrel.io' \
+    -H 'Postman-Token: 1935b69c-2fd6-4fff-b584-0458608199b8,1d75e4e4-7042-4134-a839-4cdc834a7be7' \
+    -H 'User-Agent: PostmanRuntime/7.18.0' \
+    -H 'cache-control: no-cache' \
+    -d '{
+      "fields": {
+          "environment": "Env",
+          "description": "Description here",
+          "labels": [
+              "bugfix",
+              "blitz_test"
+          ]
+      }
+  }'
 
 ## Note: must put a space after each parameter/value pair
 #appcenter test run xcuitest --app $appName --devices $deviceSetName --app-path $appPath --test-series $testSeriesName --locale "en_US" --build-dir $buildDirectory --token $appCenterLoginApiToken
